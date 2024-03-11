eacabb3f771ac483bc1e741fd742081f5d5f7ffb
jfilipepg <103340995+jfilipepg@users.noreply.github.com>
2024-03-11 09:02:10 -0300

    projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma 
+// dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), 
+// escreva um programa na linguagem que desejar onde, informado um número, 
+// ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o 
+// número informado pertence ou não a sequência.
+
+using System.Globalization;
+int numero;
+
+Console.Write ("Informe o número desejado para saber se ele pertence à sequência de fibonacci: ");
+numero = Convert.ToInt32 (Console.ReadLine ());
+
